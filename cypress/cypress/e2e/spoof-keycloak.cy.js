8f7b537db87108de96ed8704b5dd32013d5fc439
kesalohe <kesalohe@proton.me>
2022-07-21 16:31:01 +0200

    Working test with authorization code flow

--- cypress/cypress/e2e/spoof-keycloak.cy.js
+++ cypress/cypress/e2e/spoof-keycloak.cy.js
@@ -22,24 +22,30 @@ describe('Spoofing Keycloakjs', () => {
   });
 
   it('authorization code flow', () => {
-    // TODO: 1 intercept async call to keycloak to return the token.
-
     let access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJteS1mYWtlLXNlcnZlciIsImlhdCI6MTY0MDk5NTIwMCwiZXhwIjo0Nzk2NjY4ODAwLCJhdWQiOiJ3d3cuZXhhbXBsZS5jb20iLCJzdWIiOiJ0aGlzaXNmYWtlIiwiR2l2ZW5OYW1lIjoiSm9obiIsIlN1cm5hbWUiOiJEb2UifQ.lBVVksJApuw6jfg36ir5Yx24ydVvdEYsmulDFezDqdw';
-    // {"state":"a81a596b-7399-4990-84a0-bcd4bddc75b2","nonce":"0131ffe6-d6c6-4526-8d7c-84aceb3025a3","redirectUri":"http%3A%2F%2Flocalhost%3A4200%2F","expires":1653664452609}
-    const auth0State = {
-      // nonce: '0131ffe6-d6c6-4526-8d7c-84aceb3025a3',
+    cy.intercept({
+      method: 'POST',
+      url: '/realms/myrealm/protocol/openid-connect/token'
+    },
+    {
+      "access_token": access_token,
+      "token_type": "Bearer",
+      "expires_in": 3600,
+      "refresh_token": access_token
+    }).as('getToken');
+
+    const keycloakState = {
       state: 'a81a596b-7399-4990-84a0-bcd4bddc75b2',
-      redirectUri: 'http%3A%2F%2Flocalhost%3A4200%2F',
-      expires: 1969280317000
+      expires: new Date().getTime() + (60 * 60 * 1000)
     };
-    //     const callbackUrl = `http://localhost:8000/#access_token=${access_token}&session_state=9faaf148-454c-4866-a6b3-054e760edf7c&id_token=${id_token}&expires_in=${expires_in}&token_type=Bearer&state=${auth0State.state}`;
-    const callbackUrl = `http://localhost:8000/authorization-code-flow.html#access_token=${access_token}&session_state=9faaf148-454c-4866-a6b3-054e760edf7c&id_token=${access_token}&token_type=Bearer&state=${auth0State.state}`;
+    const callbackUrl = `http://localhost:8000/authorization-code-flow.html#code=42&state=${keycloakState.state}`;
     cy.visit(callbackUrl, {
       onBeforeLoad(win) {
-        win.localStorage.setItem(`kc-callback-${auth0State.state}`, JSON.stringify(auth0State));
+        win.localStorage.setItem(`kc-callback-${keycloakState.state}`, JSON.stringify(keycloakState));
       }
     });
 
-    // cy.visit('http://localhost:4200')
+    cy.get('#givenName').contains('John');
+    cy.get('#surname').contains('Doe');
   });
 })
